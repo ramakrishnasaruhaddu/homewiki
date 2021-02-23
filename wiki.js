cdc1d4a1099f34428cba137e0422b577ce371143
ramakrishnasarihaddu <ramakrishnasri11@gmail.com>
2021-02-23 12:05:31 +0530

    modifcation

--- wiki.js
+++ wiki.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/homewiki'));
+app.use(express.static('./dist/wiki/server'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.js', {root: 'dist/homewiki/'}),
+    res.sendFile('index.js', {root: 'dist/wiki/server'}),
 );
 
 // Start the app by listening on the default Heroku port
